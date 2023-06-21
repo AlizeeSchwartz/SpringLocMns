dd46ebd5455e5dbda5a374259e2fa655497e170c
Alizée Schwartz <alizeeschwartz67@gmail.com>
2023-06-21 23:12:31 +0200

    dockercompose

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 git pull
 
 # Construire le projet avec Maven
-bash mvnw  --settings /home/debian/.m2/settings.xml package
+bash mvnw  --settings /home/debian/.m2/settings.xml package -DskipTests
 
 # Construire l'image Docker
 docker build --no-cache -t image-loc-alizee .
